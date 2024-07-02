2e531325d7dd56076a95874d2b7d0d48f0320837
Tantos Alexandra <alexandra.tantos@yahoo.com>
2024-07-02 19:39:42 +0300

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             Welcome to my digital domain! I am a passionate and versatile web
             developer dedicated to crafting immersive online experiences. With a
             keen eye for detail and a love for elegant, functional design, I
-            bring ideas to life in the digital realm. My journey in web
+            bring ideas to life in the digital world. My journey in web
             development has equipped me with a diverse skill set that spans
             front-end and back-end technologies.{" "}
           </p>
@@ -21,15 +21,15 @@ const About = () => {
             my expertise extends to popular frameworks like React, where I excel
             in building interactive and engaging user interfaces. On the server
             side, I am well-versed in utilizing Node.js and Express to create
-            robust and scalable back-end solutions.{" "}
+            scalable back-end solutions.{" "}
           </p>
           <p className="text-slate-600 mt-8 leading-loose">
-            Collaborative by nature, I thrive in team environments where ideas
-            flourish, and innovation takes center stage. My goal is to not only
-            meet project requirements but to exceed expectations by delivering
-            solutions that seamlessly blend creativity and functionality.
-            Explore my portfolio to witness the fusion of my technical skills,
-            creativity, and passion for web development.
+            Collaborative by nature, I like working in team environments where
+            ideas flourish, and innovation takes center stage. My goal is to not
+            only meet project requirements but to exceed expectations by
+            delivering solutions that blend creativity and functionality.
+            Explore my portfolio to explore my technical skills, creativity, and
+            passion for web development.
           </p>
         </article>
       </div>
