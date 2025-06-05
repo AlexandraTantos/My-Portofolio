5f986d1b599c553d7a797b86a758ec409ac11195
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-06-05 21:00:51 +0300

    Updated ProjectsCard

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -14,12 +14,16 @@ const ProjectsCard = ({ url, img, github, title, text }) => {
         <h2 className="text-xl tracking-wide font-medium">{title}</h2>
         <p className="mt-4 text-slate-700 leading-loose">{text}</p>
         <div className="mt-4 flex gap-x-4">
-          <a href={url}>
-            <TbWorldWww className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
-          </a>
-          <a href={github}>
-            <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
-          </a>
+          {url && (
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              <TbWorldWww className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            </a>
+          )}
+          {github && (
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            </a>
+          )}
         </div>
       </div>
     </article>
@@ -27,9 +31,9 @@ const ProjectsCard = ({ url, img, github, title, text }) => {
 };
 
 ProjectsCard.propTypes = {
-  url: PropTypes.string.isRequired,
+  url: PropTypes.string,
   img: PropTypes.string.isRequired,
-  github: PropTypes.string.isRequired,
+  github: PropTypes.string,
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
 };
